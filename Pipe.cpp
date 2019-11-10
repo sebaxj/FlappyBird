4713b45f288fb5fbfa8a2971ed4b87d9186cb402
rshiv2 <56366401+rshiv2@users.noreply.github.com>
2019-11-09 23:39:38 -0800

    Simplify the way we express the obstacle's opening

--- Pipe.cpp
+++ Pipe.cpp
@@ -9,12 +9,28 @@
 Pipe::Pipe(int startX) {
   this->vx = -1/1500.0;
   this->x = startX;
-  this->baseRange = 3;
-  generateRandomBase();
+  this->gapRange = 3;
+  generateRandomGap();
 }
 
-Pipe::generateRandomBase() {
-  this->base = random(0, 6);  // the base of any obstacle will be 0-5
+
+void Pipe::generateRandomGap() {
+  int base = 0;//random(0, 6);  // the base of any obstacle will be 0-5
+  this->gapBase = base;
+}
+
+int Pipe::getGapBase() {
+  return this->gapBase;
+}
+
+int Pipe::getGapRange() {
+  return this->gapRange;
+}
+
+void Pipe::printGap() {
+  for (int i = 0; i < this->gapRange; i++) {
+    Serial.print(this->gapBase + i); Serial.print(" ");
+  }
 }
 
 void Pipe::setVel(float newVx) {
@@ -33,18 +49,3 @@ int Pipe::getX() {
   return this->x;
 }
 
-void Pipe::setBase(int y) {
-  this->base = y;
-}
-
-int Pipe::getBase() {
-  return this->base;
-}
-
-void Pipe::setOpening(int y) {
-  this->baseRange = y;
-}
-
-int Pipe::getOpening() {
-  return this->baseRange;
-}
